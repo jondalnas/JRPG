02a9852f67d0fb3c90b08e6880ece57a391f0eb2
Jonas Jensen <jdudal11@gmail.com>
2019-11-08 09:22:17 +0100

    Added graphics

--- src/com/JoL/JRPG/Main.java
+++ src/com/JoL/JRPG/Main.java
@@ -1,10 +1,113 @@
 package com.JoL.JRPG;
 
-public class Main {
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class Main extends Canvas implements Runnable {
+	public static int WIDTH = 640, HEIGHT = 480;
+	
+	private boolean running;
+	private Thread thread;
+	
+	private Screen screen;
+	
+	public Main() {
+		Dimension size = new Dimension(WIDTH, HEIGHT);
+		 
+		setSize(size);
+		 
+		screen = new Screen(WIDTH, HEIGHT);
+	}
+	
+	public synchronized void start() {
+		if (running) return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+ 
+	public synchronized void stop() {
+		if (!running) return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
+	static double deltaTime = 0;
+	public void run() {
+		long last = System.nanoTime();
+		byte frames = 0;
+		double accDeltaTime = 0;
+		while(running) {
+			deltaTime = System.nanoTime() - last;
+			accDeltaTime += deltaTime;
+			
+			tick();
+			render();
+			
+			frames++;
+			if (frames > 60) {
+				try {
+					Thread.sleep((int) ((1.0 - (accDeltaTime > 1 ? 1 : accDeltaTime))*1000.0));
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				accDeltaTime = 0;
+			}
+		}
+	}
+	
+	public static double deltaTime() {
+		return deltaTime;
+	}
+	 
+	private void tick() {
+	}
+ 
+	private void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		screen.render(g);
+		
+		g.dispose();
+		bs.show();
+	}
+	
+	public static void main(String[] args) {
+		Main game = new Main();
+		
+		JFrame frame = new JFrame("Graphics Engine");
+		JPanel panel = new JPanel(new BorderLayout());
+		
+		panel.add(game, 0);
+		
+		frame.setContentPane(panel);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		 
+		frame.setSize(new Dimension(frame.getWidth(), frame.getHeight()));
+		frame.setPreferredSize(frame.getSize());
+		
+		game.start();
+	}
 }
---
+++ src/com/JoL/JRPG/Screen.java
@@ -0,0 +1,20 @@
+package com.JoL.JRPG;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Screen {
+	public int width, height;
+	
+	public Screen(int w, int h) {
+		width = w;
+		height = h;
+	}
+
+	public void render(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(0, 0, width, height);
+		g.setColor(Color.blue);
+		g.fillRect(50, 50, width-50, height-50);
+	}
+}
